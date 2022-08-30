c797eecb184b19145bc4a39acc24fd11aeb41b6f
qudwls5771 <qudwls5771@naver.com>
2022-08-30 14:42:08 +0900

    사업자, 회원 날짜 = 회원가입,수정 날짜는 어노테이션을 넣고 탈퇴날짜는 넣지 않는다.

--- Team_pro_ex/src/main/java/com/example/team_pro_ex/TeamProExApplication.java
+++ Team_pro_ex/src/main/java/com/example/team_pro_ex/TeamProExApplication.java
@@ -2,7 +2,9 @@ package com.example.team_pro_ex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TeamProExApplication {
 
