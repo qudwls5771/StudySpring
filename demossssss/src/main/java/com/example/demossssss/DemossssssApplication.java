fac53a691e64630831b357e3f94f698707ae073d
qudwls5771 <qudwls5771@naver.com>
2022-09-04 02:55:45 +0900

    테스트

---
+++ demossssss/src/main/java/com/example/demossssss/DemossssssApplication.java
@@ -0,0 +1,13 @@
+package com.example.demossssss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemossssssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemossssssApplication.class, args);
+	}
+
+}
---
+++ demossssss/src/test/java/com/example/demossssss/DemossssssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demossssss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemossssssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
